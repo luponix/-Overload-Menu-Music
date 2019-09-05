00ba7fc20e1be9c80a2a91e0b56f18800731e8fe
Lukas Denda <34916038+luponix@users.noreply.github.com>
2019-09-05 13:34:32 +0200

    Add files via upload

---
+++ RMMTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace Mod_ReplaceMenuMusic
+{
+   public static class RMMTimer
+    {
+        static Timer _timer;
+       
+        public static void Start()
+        {
+            // Set up the timer for 3 seconds.
+            var timer = new Timer(1000);
+            // To add the elapsed event handler:
+            // ... Type "_timer.Elapsed += " and press tab twice.
+            timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            timer.Enabled = true;
+            _timer = timer;
+
+        }
+        static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Control.current_song_pointer += 1;
+            //uConsole.Log("song time: "+Control.current_song_pointer); 
+            if(Control.current_song_pointer >= Control.current_song_length)
+            {
+                TimerStop();
+                Methods.SwitchToNextSong();
+            }
+        }
+
+        public static void TimerStop()
+        {
+            _timer.Stop();
+            Control.current_song_pointer = 0;
+        }
+
+
+       
+    }
+}
